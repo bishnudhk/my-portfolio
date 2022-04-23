fc995b024c87ce825ef04b834e0e43a4e68220ef
Bishnu Dhakal <bishnudhakal113@gmail.com>
2022-04-23 20:52:33 +0545

    commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./About.css";
 import ME from "../../components/images/About.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { ImFolderOpen } from "react-icons/im";
 
 const About = () => {
   return (
@@ -16,10 +19,23 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>2+ years Working</small>
             </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Client</h5>
+              <small>2+ Client</small>
+            </article>
+            <article className="about__card">
+              <ImFolderOpen className="about__icon" />
+              <h5>Projected</h5>
+              <small>5+ project work</small>
+            </article>
           </div>
+          <p>I am a Bishnu Dhakal.I am a front end developer .I read in Nepal college of Information Technology affilated to Pokhara university.</p>
+         <a href="#content " className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,12 +1,259 @@
-import React from 'react';
-import './Services.css';
+import React from "react";
+import "./Services.css";
+import { AiOutlineCheck } from "react-icons/ai";
 
 const Services = () => {
   return (
-    <section id='services'>
-      Services
-    </section>
-  )
-}
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Graphics Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* web development */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                In today's Era having a website is very important for any
+                business. It is no longer possible for the businesses to see
+                growth in their market share unless they have a proper web
+                application. Web Application plays crucial in Branding Process.
+                Maintaining proper communication channel between potential
+                Customers and business organization will be more easier. The
+                current scenario in the world of business has become so
+                intensely competitive that is has become more important to have
+                own web application. So, Develop your web application with us.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                In today's Era having a website is very important for any
+                business. It is no longer possible for the businesses to see
+                growth in their market share unless they have a proper web
+                application. Web Application plays crucial in Branding Process.
+                Maintaining proper communication channel between potential
+                Customers and business organization will be more easier. The
+                current scenario in the world of business has become so
+                intensely competitive that is has become more important to have
+                own web application. So, Develop your web application with us.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+                We offer a complete graphic design service that covers every
+                possible area of the art. We have wrapped buildings in graphics,
+                created three-dimensional press kits to woo journalists, we have
+                even created a virtual store, complete with virtual stock; but
+                to be a little more down-to-earth, we design what you need –
+                business cards, corporate literature, press kits, exhibition
+                graphics, packaging, annual reports and promotional materials.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* content creation  */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+              </p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>
+              Are you looking to make your website's content easier to manage? Various web development companies use content management systems as a customized web application for managing their websites. XDezo Technologies provides enhanced CMS support with web integration of multiple CMS platforms including transactional, e-commerce, content-based CMS.
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
